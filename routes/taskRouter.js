c641e367407ebab76964f122aa0d225e748894a0
PriyeBriggs <depriyebriggs177@gmail.com>
2024-07-28 15:21:06 +0100

    first commit

---
+++ routes/taskRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const { createTask, getAllTask, editTask, deleteTask,} = require("../controllers/taskController");
+
+const router = express.Router();
+
+router.post("/create", createTask);
+
+router.get("/get", getAllTask);
+
+router.patch("/:id", editTask );
+
+router.delete("/:id", deleteTask );
+
+module.exports = router; 
